38d01de525aa49c4ac85eb5f93736855869a15f9
DispicableLee <lee.robert053@gmail.com>
2023-11-08 11:32:53 -0500

    copypaste

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,55 +3,63 @@
 # Table name: users
 #
 #  id              :bigint           not null, primary key
-#  username        :string           not null
 #  email           :string           not null
-#  session_token   :string           not null
+#  username        :string           not null
 #  password_digest :string           not null
-#  bio             :string
+#  session_token   :string           not null
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
-    # figvapebr
-    before_validation :ensure_session_token
-    validates :username, :session_token, presence: true, uniqueness: true
-    validates :password_digest, presence: true
-    validates :password, length: {minimum: 6, allow_nil: true}
 
+  has_secure_password
+  before_validation :ensure_session_token
+  validates :username, 
+    uniqueness: true, 
+    length: { in: 3..30 }, 
+    format: { without: URI::MailTo::EMAIL_REGEXP, message:  "can't be an email" }
+  validates :email, 
+    uniqueness: true, 
+    length: { in: 3..255 }, 
+    format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :session_token, presence: true, uniqueness: true
+  validates :password, length: { in: 6..255 }, allow_nil: true
 
-    attr_reader :password
+    def self.find_by_credentials(credential, password)
+      isEmail = false
+      if credential.match(URI::MailTo::EMAIL_REGEXP)
+        isEmail = true
+        user = User.find_by(email: credential)
+      end
 
+      
+      user = User.find_by(username: credential)
 
-    def self.find_by_credentials(username, password)
-        @user = User.find_by(username: username)
-        if @user && @user.is_password?(password)
-            @user
-        else
-            nil
-        end
+      if user&.authenticate(password)
+        return user
+      else 
+        nil
+      end
     end
-
-    def password=(password)
-        @password = password
-        self.password_digest = BCrypt::Password.create(password)
+  
+    def generate_unique_session_token
+      loop do 
+        token = SecureRandom.urlsafe_base64
+        return token if !User.exists?(session_token: token)
+      end
     end
-
-    def is_password?(password)
-        BCrypt::Password.new(self.password_digest).is_password?(password)
-    end
-
-    def reset_session_token!
-        self.session_token = generate_session_token
-        self.save!
-        self.session_token
-    end
-
-    private
-    def generate_session_token
-        SecureRandom::urlsafe_base64
-    end
-
+  
     def ensure_session_token
-        self.session_token ||= generate_session_token
+      self.session_token ||= generate_unique_session_token
     end
+    
+    def reset_session_token!
+      self.session_token = generate_unique_session_token
+      self.save!
+      self.session_token
+    end
+
+
+
+
 end
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import configureStore from './store';
+import csrfFetch, { restoreCSRF } from './store/csrf';
+import * as sessionActions from "./store/session"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const initialState = {}
+
+const store = configureStore(initialState);
+
+window.store = store
+
+function Root() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+}
+
+const renderApplication = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Root />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+if (
+  sessionStorage.getItem("currentUser") === null ||
+  sessionStorage.getItem("X-CSRF-Token") === null 
+) {
+  store.dispatch(sessionActions.restoreSession()).then(renderApplication);
+} else {
+  renderApplication();
+}
