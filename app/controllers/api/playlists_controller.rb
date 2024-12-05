2d6aea3c6a16d3860016837d4880694cf5ff7b6e
DispicableLee <lee.robert053@gmail.com>
2024-12-05 12:13:48 -0500

    playlist

--- app/controllers/api/playlists_controller.rb
+++ app/controllers/api/playlists_controller.rb
@@ -11,7 +11,25 @@ class Api::PlaylistsController < ApplicationController
 
   def create
     puts "Params: #{params.inspect}"
+    debugger
+    @playlist = Playlist.new(playlist_params)
+    if @playlist.save
+      if params[:playlist][:track_objects].present?
+        params[:playlist][:track_objects].each do |track_object|
+          @playlist.tracks.build(track_object.permit(:title, :url, :uploader_id))
+        end
+      end
+      render :show
+    else
+      render json: @playlist.errors.full_messages, status: 422
+    end
+  end
 
+  def update
+    @playlist = Playlist.find(params[:id])
+    if @playlist.update(playlist_params)
+
+    end
   end
 
   private
